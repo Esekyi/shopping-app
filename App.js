97f97aaac0e6e85640057a89d28c2366061f06ec
Emmanuel Sekyi <52067244+Esekyi@users.noreply.github.com>
2021-10-22 23:03:40 +0000

    update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style{{fontWeight: "900"}}>Hello World!!</Text>
       <StatusBar style="auto" />
     </View>
   );
