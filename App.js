675d122ada697f66c67429db0fbea36cb553b005
[esekyi] <[sskert10@gmail.com]>
2021-10-30 00:10:02 +0000

    [Progress䷢]imported Montserrat fonts

--- App.js
+++ App.js
@@ -3,11 +3,32 @@ import React from 'react';
 import HomePage from './Screens/HomPage';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import {
   useFonts,
   NotoSerif_400Regular,
   NotoSerif_700Bold,
 } from '@expo-google-fonts/noto-serif';
+import {
+  Montserrat_100Thin,
+  Montserrat_100Thin_Italic,
+  Montserrat_200ExtraLight,
+  Montserrat_200ExtraLight_Italic,
+  Montserrat_300Light,
+  Montserrat_300Light_Italic,
+  Montserrat_400Regular,
+  Montserrat_400Regular_Italic,
+  Montserrat_500Medium,
+  Montserrat_500Medium_Italic,
+  Montserrat_600SemiBold,
+  Montserrat_600SemiBold_Italic,
+  Montserrat_700Bold,
+  Montserrat_700Bold_Italic,
+  Montserrat_800ExtraBold,
+  Montserrat_800ExtraBold_Italic,
+  Montserrat_900Black,
+  Montserrat_900Black_Italic,
+} from '@expo-google-fonts/montserrat';
 
 const Stack = createNativeStackNavigator();
 
@@ -15,7 +36,26 @@ const App = () => {
   let [fontsLoaded] = useFonts({
     NotoSerif_400Regular,
     NotoSerif_700Bold,
+    Montserrat_100Thin,
+    Montserrat_100Thin_Italic,
+    Montserrat_200ExtraLight,
+    Montserrat_200ExtraLight_Italic,
+    Montserrat_300Light,
+    Montserrat_300Light_Italic,
+    Montserrat_400Regular,
+    Montserrat_400Regular_Italic,
+    Montserrat_500Medium,
+    Montserrat_500Medium_Italic,
+    Montserrat_600SemiBold,
+    Montserrat_600SemiBold_Italic,
+    Montserrat_700Bold,
+    Montserrat_700Bold_Italic,
+    Montserrat_800ExtraBold,
+    Montserrat_800ExtraBold_Italic,
+    Montserrat_900Black,
+    Montserrat_900Black_Italic,
   });
+
 
   if (!fontsLoaded) {
     return <View />;
