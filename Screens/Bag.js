f21bea1b369cc9b0a6a5b2b832fcbafc5b86aaad
[esekyi] <[sskert10@gmail.com]>
2021-11-01 02:58:03 +0000

    [Progress🧑🏽‍🦯]Added a third screen, Cart screen

---
+++ Screens/Bag.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Bag = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    
+})
+export default Bag;
