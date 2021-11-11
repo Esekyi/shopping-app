df4df2ed023add154f1e4b5675f1359247b09e28
[esekyi] <[sskert10@gmail.com]>
2021-11-11 03:30:14 +0000

    [Edit✍🏼]Added color object

--- Consts/cardItemList.js
+++ Consts/cardItemList.js
@@ -4,81 +4,155 @@ const storeItems = [
       name: 'Pre Plucked Virgin Hair Body Wave HD Lace Closure Wig',
       price: '200.99',
       favorite: true,
-      images: {imageMain: require('../assets/1.png'), imageBrown: require('../assets/1_Bown.png')},
+    images:
+    {
+      imageMain: require('../assets/1.png'),
+      black: require('../assets/1.png'),
+      sienna: require('../assets/1_Bown.png'),
+      // violet: require('../assets/1_Bown.png'),
+    },
       productNumber:'78102927301',
+      stock: 10,
       description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "Lorem Ipsum is simply dummy text \n of the printing and typesetting industry. \n Lorem Ipsum has been the industry's \n standard dummy text ever since the 1500s,\nwhen an unknown printer took a galley of type \n and scrambled it to make a type specimen book.",
   },
    {
       id: 2,
       name: 'T-Shirts for Men | Round Neck with graphic front',
       price: '235.99',
       favorite: false,
-      images: { imageMain: require('../assets/11.png'), imageBrown: require('../assets/12_Brown.png') },
+     images: {
+       imageMain: require('../assets/11.png'),
+       black: require('../assets/11.png'),
+       darkslategray: require('../assets/straightBlack.jpg')
+     },
       productNumber:'6727891800',
+      stock: 10,
       description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "Lorem Ipsum is simply dummy text \n of the printing and typesetting industry. \n Lorem Ipsum has been the industry's \n standard dummy text ever since the 1500s,\nwhen an unknown printer took a galley of type \n and scrambled it to make a type specimen book.",
     },
     {
       id: 3,
       name: 'T-Shirts for Men | Round Neck with graphic front',
       price: '235.99',
       favorite: false,
-      images: { imageMain: require('../assets/12.png'), imageBrown: require('../assets/12_Brown.png') },
+      images: {
+        imageMain: require('../assets/lightBrownShirt.webp'),
+        wheat: require('../assets/lightBrownShirt.webp'),
+        lightsalmon: require('../assets/lightsalmon.webp'),
+      },
       productNumber:'5536728920',
+      stock: 80,
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  },
+    {
+      id: 4,
+      name: 'Pre Plucked | brazillian body curls - bundles',
+      price: '235.99',
+      favorite: false,
+      images: {
+        imageMain: require('../assets/12.png'),
+        darkgray: require('../assets/12.png'),
+        black: require('../assets/4.png'),
+      },
+      productNumber:'6209827378',
+      stock: 120,
       description:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     },
   
     {
-      id: 4,
+      id: 5,
       name: 'Hand-made frontal wig | Human virgin hair',
       price: '125.99',
-      favorite: true,
-      images: { imageMain: require('../assets/4.png') },
+      favorite: false,
+      images: {
+        imageMain: require('../assets/4.png'),
+        black: require('../assets/4.png'),
+        wheat:require('../assets/blonde.png'),
+      },
       productNumber:'2018727891',
+      stock: 40,
       description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    },
-    {
-      id: 5,
-      name: 'Brazillian fringe - Pure Virgin Hair',
-      price: '500.99',
-      favorite: true,
-      images: { imageMain: require('../assets/5.png') },
-      productNumber:'5201927993',
-      description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "Lorem Ipsum is simply dummy text \n of the printing and typesetting industry. \n Lorem Ipsum has been the industry's \n standard dummy text ever since the 1500s,\nwhen an unknown printer took a galley of type \n and scrambled it to make a type specimen book.",
     },
     {
       id: 6,
-      name: 'Classic T-Shirt For men - Black buttons',
-      price: '150.99',
-      favorite: false,
-      images: { imageMain: require('../assets/7.png') },
-      productNumber:'2700917537',
+      name: 'Brazillian fringe - Pure Virgin Hair',
+      price: '500.99',
+      favorite: true,
+      images: {
+        imageMain: require('../assets/5.png'),
+        black: require('../assets/5.png'),
+        darkgreen: require('../assets/green.jpg')
+        
+      },
+      productNumber:'5201927993',
+      stock: 10,
       description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "Lorem Ipsum is simply dummy text \n of the printing and typesetting industry. \n Lorem Ipsum has been the industry's \n standard dummy text ever since the 1500s,\nwhen an unknown printer took a galley of type \n and scrambled it to make a type specimen book.",
     },
     {
       id: 7,
-      name: 'Virgin Fringe - Tanious Hair wig',
+      name: 'Classic T-Shirt For men - Black buttons',
       price: '150.99',
       favorite: true,
-    images:{ imageMain: require('../assets/6.png') },
-    productNumber:'7092828883',
-    description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      images: {
+        imageMain: require('../assets/blueShirt.webp'),
+        blue: require('../assets/blueShirt.webp'),
+        pink: require('../assets/pinkShirt.webp'),
+
+      },
+      productNumber:'2700917537',
+      stock: 20,
+      description:
+        "Lorem Ipsum is simply dummy text \n of the printing and typesetting industry. \n Lorem Ipsum has been the industry's \n standard dummy text ever since the 1500s,\nwhen an unknown printer took a galley of type \n and scrambled it to make a type specimen book.",
     },
     {
       id: 8,
+      name: 'Virgin Fringe - Tanious Hair wig',
+      price: '150.99',
+      favorite: false,
+      images: {
+        imageMain: require('../assets/6.png'),
+        black: require('../assets/6.png'),
+        saddlebrown: require('../assets/fringebrown.png')
+      },
+    productNumber:'7092828883',
+    stock: 19,
+    description:
+        "Lorem Ipsum is simply dummy text \n of the printing and typesetting industry. \n Lorem Ipsum has been the industry's \n standard dummy text ever since the 1500s,\nwhen an unknown printer took a galley of type \n and scrambled it to make a type specimen book.",
+    },
+    {
+      id: 9,
       name: 'Silky Straight Human Hair Full Lace Wig',
       price: '290.99',
       favorite: false,
-      images: { imageMain: require('../assets/8.png'), imageBlack: require('../assets/8_Black.png'), imageBlonde:require('../assets/8_Blonde.png') },
+      images: {
+        imageMain: require('../assets/8_Black.png'),
+        black: require('../assets/8_Black.png'),
+        sienna: require('../assets/1_Bown.png')
+      },
       productNumber:'1001990112',
+      stock: 32,
       description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "Lorem Ipsum is simply dummy text \n of the printing and typesetting industry. \n Lorem Ipsum has been the industry's \n standard dummy text ever since the 1500s,\nwhen an unknown printer took a galley of type \n and scrambled it to make a type specimen book.",
+  },
+    {
+      id: 10,
+      name: 'Silky Straight Human Hair Full Lace Wig',
+      price: '290.99',
+      favorite: true,
+      images: {
+        imageMain: require('../assets/8_Black.png'),
+        black: require('../assets/8_Black.png'),
+        darkred: require('../assets/straight_wig_nadula.png')
+      },
+      productNumber:'1001990112',
+      stock: 50,
+      description:
+        "Lorem Ipsum is simply dummy text \n of the printing and typesetting industry. \n Lorem Ipsum has been the industry's \n standard dummy text ever since the 1500s,\nwhen an unknown printer took a galley of type \n and scrambled it to make a type specimen book.",
     },
   ];
   
